e4d77e56b32beb65f79b14318d6aec5b05feb026
Atharva T <atharva.talaghatti@ats-group.com>
2026-05-18 12:13:45 +0530

    all

---
+++ src/main/java/com/spring/ai/firstproject/ThisFirstProjectForSpingAndAiCommunciatiomApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ai.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisFirstProjectForSpingAndAiCommunciatiomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisFirstProjectForSpingAndAiCommunciatiomApplication.class, args);
+	}
+
+}
